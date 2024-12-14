5f9b36c811c9f09b22ab28073885429fa6b2e90f
mirai-fp <gill041114@gmail.com>
2024-12-15 06:52:36 +0900

    corp
    corp

---
+++ html_fplus/js/custom.js
@@ -0,0 +1,37 @@
+// Hamburger menu toggle
+jQuery('.hamburger').click(function(){
+  jQuery('.sp-menu').toggleClass('active');
+  jQuery('.hamburger').toggleClass('open');
+});
+
+jQuery('.header__nav__item').click(function(){
+  jQuery('.sp-menu').removeClass('active');
+  jQuery('.hamburger').removeClass('open');
+});
+
+jQuery('.header__contact').click(function(){
+  jQuery('.sp-menu').removeClass('active');
+  jQuery('.hamburger').removeClass('open');
+});
+
+// fadein animation
+jQuery(window).on('scroll', function() {
+    jQuery('.fade-in').each(function() {
+      var pos = jQuery(this).offset().top;
+      var scroll = jQuery(window).scrollTop();
+      var wHeight = jQuery(window).height();
+      
+      if (scroll > pos - wHeight + (wHeight / 80)) {
+        jQuery(this).addClass('inview');
+      }
+    });
+});
+
+// loading
+const loading = document.querySelector(".loading");
+window.addEventListener("load", () => {
+setTimeout(() => {
+    loading.classList.add("loaded");
+}, 3000);
+//3秒後にローディング画面非表示
+});
